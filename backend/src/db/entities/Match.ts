6eb551e59cf6f8b57944afc59b3d026f691d9d74
Anamika Nayak <anayak@pdx.edu>
2023-04-30 00:07:32 -0700

    Fixed ReferenceError: Cannot access 'User' before initialization

--- backend/src/db/entities/Match.ts
+++ backend/src/db/entities/Match.ts
@@ -2,6 +2,7 @@
 import { Entity, Property, Unique, ManyToOne } from "@mikro-orm/core";
 import { BaseEntity } from "./BaseEntity.js";
 import { User } from "./User.js";
+import type {Rel} from "@mikro-orm/core";
 
 
 @Entity()
@@ -10,16 +11,14 @@ export class Match {
 	// The person who performed the match/swiped right
 	//one owner could have many matches, many ties to the class and to ties to the field
 	@ManyToOne({primary: true})
-	owner!: User;
+	owner!: Rel<User>;
 
 	// The account whose profile was swiped-right-on
 	//one user account to be able to matched by lots of other people
 	@ManyToOne({primary: true})
-	matchee!: User;
+	matchee!: Rel<User>;
 
 	@Property()
 	created_at = new Date();
 
 }
-
-
